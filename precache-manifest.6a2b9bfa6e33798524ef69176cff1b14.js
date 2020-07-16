67d21465a5ff400cccdd4eccdaafe25a77e1debc
Simran Raj <simran.raj@Triconinfotech.com>
2020-07-16 18:11:22 +0530

    Updates

---
+++ precache-manifest.6a2b9bfa6e33798524ef69176cff1b14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d7445dba67360f6ae0f8e3c3be6c467",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "27968206b19aacd77bf2",
+    "url": "/monsters/static/css/main.4f6fd437.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27968206b19aacd77bf2",
+    "url": "/monsters/static/js/main.d79492d7.chunk.js"
+  },
+  {
+    "revision": "03919b83b878cef951f1",
+    "url": "/monsters/static/js/runtime-main.cc0b5fe4.js"
+  }
+]);
